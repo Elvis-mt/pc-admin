c13c36fd6848718849bd2413385adc0d7eebed9e
matao <17693120506@163.com>
2020-09-10 13:11:21 +0800

    登录路由，sass配置，login页面初步搭建

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,6 +36,7 @@
 export default {
   name: 'HelloWorld',
   props: {
+    /* eslint-disable */
     msg: String
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
-
-const routes = [
+Vue.use(Router)
+/**
+ * 不变路由
+ * 没有权限要求的基本页面
+ * 所有角色都可以访问
+ */
+export const constantRoutes = [
+  // 登录
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/views/Home.vue')
   },
   {
     path: '/about',
@@ -16,14 +25,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+const createRouter = () => new Router({
+  // mode: 'history',
+  // base: process.env.BASE_URL,
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
-
+const router = createRouter()
 export default router
